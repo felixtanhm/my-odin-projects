08493b698ecbc8757c15e8e08ec4dbecff3a4a70
felixtanhm <felixtanhm@gmail.com>
2024-02-11 17:05:14 +0800

    Error 404 handling

--- full-stack-javascript/basic-info-site/index.js
+++ full-stack-javascript/basic-info-site/index.js
@@ -3,24 +3,30 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/")
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
+  const filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : `${req.url}`
+  );
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end(`Server Error: ${err.code}`);
       }
-    );
-  if (req.url !== "/")
-    fs.readFile(
-      path.join(__dirname, "public", "about.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || "8080";
