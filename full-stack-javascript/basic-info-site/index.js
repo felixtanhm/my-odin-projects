0ebd49b4b632b2b40db9854589e366c68ee61515
felixtanhm <felixtanhm@gmail.com>
2024-02-11 16:12:05 +0800

    Created server & basic html pages

--- full-stack-javascript/basic-info-site/index.js
+++ full-stack-javascript/basic-info-site/index.js
@@ -1,3 +1,14 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const pathURL = req.url;
+  console.log(pathURL);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("hello world");
+});
+
+const PORT = process.env.PORT || "8080";
+
+server.listen(PORT, () => console.log("server is running"));
