6338cd401bdecd471fe233c3e110d150be9e2382
Felix Hong Ming Tan <feltan@c2r6s11.42singapore.sg>
2024-03-09 13:11:21 +0800

    submit logic and final styling

--- full-stack-javascript/sign-up-form/script.js
+++ full-stack-javascript/sign-up-form/script.js
@@ -5,12 +5,11 @@ const successDiv = document.getElementById('success-div');
 form.addEventListener('submit', event => {
 	event.preventDefault();
 	submitBtn.textContent = "Submitting"
+	submitBtn.setAttribute('disabled', true)
 	setTimeout(() => {
 		successDiv.classList.remove('hidden')
+		submitBtn.removeAttribute('disabled')
 		submitBtn.textContent = "Submit"
 		form.reset()
 	}, 1000)
-	setTimeout(() => {
-		successDiv.classList.add('hidden')
-	}, 2500)
 })
