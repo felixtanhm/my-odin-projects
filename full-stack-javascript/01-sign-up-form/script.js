88791329fae789e2111665656ed00e5419bddc15
felixtanhm <felixtanhm@gmail.com>
2024-03-09 21:04:57 +0800

    code format

--- full-stack-javascript/01-sign-up-form/script.js
+++ full-stack-javascript/01-sign-up-form/script.js
@@ -1,15 +1,15 @@
-const form = document.getElementById('form');
-const submitBtn = document.getElementById('submit');
-const successDiv = document.getElementById('success-div');
+const form = document.getElementById("form");
+const submitBtn = document.getElementById("submit");
+const successDiv = document.getElementById("success-div");
 
-form.addEventListener('submit', event => {
-	event.preventDefault();
-	submitBtn.textContent = "Submitting"
-	submitBtn.setAttribute('disabled', true)
-	setTimeout(() => {
-		successDiv.classList.remove('hidden')
-		submitBtn.removeAttribute('disabled')
-		submitBtn.textContent = "Submit"
-		form.reset()
-	}, 1000)
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  submitBtn.textContent = "Submitting";
+  submitBtn.setAttribute("disabled", true);
+  setTimeout(() => {
+    successDiv.classList.remove("hidden");
+    submitBtn.removeAttribute("disabled");
+    submitBtn.textContent = "Submit";
+    form.reset();
+  }, 1000);
+});
