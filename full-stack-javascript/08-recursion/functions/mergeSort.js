137fde2711a9bbf1673e2b368939d15710b00b9d
felixtanhm <felixtanhm@gmail.com>
2024-03-12 00:18:27 +0800

    implement merge sort

--- full-stack-javascript/08-recursion/functions/mergeSort.js
+++ full-stack-javascript/08-recursion/functions/mergeSort.js
@@ -6,12 +6,38 @@ function split(numArr) {
   const leftArr = numArr.slice(0, mid);
   const rightArr = numArr.slice(mid, numArr.length);
 
-  mergeSort(split(leftArr), split(rightArr));
+  return mergeSort(split(leftArr), split(rightArr));
 }
 
 function mergeSort(arr1, arr2) {
-  console.log(arr1);
-  console.log(arr2);
+  if (arr1 === undefined) return arr2;
+  if (arr2 === undefined) return arr1;
+
+  const result = [];
+  let l = 0;
+  let r = 0;
+
+  while (l < arr1.length && r < arr2.length) {
+    if (arr1[l] < arr2[r]) {
+      result.push(arr1[l]);
+      l++;
+    } else {
+      result.push(arr2[r]);
+      r++;
+    }
+  }
+
+  while (l < arr1.length) {
+    result.push(arr1[l]);
+    l++;
+  }
+
+  while (r < arr2.length) {
+    result.push(arr2[r]);
+    r++;
+  }
+
+  return result;
 }
 
-split([0, 1, 2, 3, 4]);
+console.log(split([5, 3, 4, 1, 2]));
