b770876185c4e94fa776586272ea3a26280d71b1
felixtanhm <felixtanhm@gmail.com>
2024-04-15 18:10:02 +0800

    basic styling

--- full-stack-javascript/20-forms-and-deployment/controllers/bookController.js
+++ full-stack-javascript/20-forms-and-deployment/controllers/bookController.js
@@ -36,7 +36,12 @@ exports.index = async function (req, res, next) {
 // Display list of all books.
 exports.book_list = async function (req, res, next) {
   try {
-    res.send("NOT IMPLEMENTED: Book list");
+    const allBooks = await Book.find({}, "title author")
+      .sort({ title: 1 })
+      .populate("author")
+      .exec();
+
+    res.render("book_list", { title: "Book List", book_list: allBooks });
   } catch (error) {
     return next(error);
   }
